4c4fdf1a713ac4a6b2c57f39a6f864d58ea24221
Vasili <v.staratsitarau@gmail.com>
2022-09-16 11:13:42 +0200

    remove search logic in extension

--- DataFromJSONfile/Extensions/Ex+TVC+search.swift
+++ DataFromJSONfile/Extensions/Ex+TVC+search.swift
@@ -26,5 +26,55 @@ extension CityTable: UISearchBarDelegate {
         searchBar.text = ""
         getFilteredDataBinary(searchedText: "")
     }
+    // MARK: - Search Logic: Filter
+    func getFilteredData(searchedText: String = "") {
+        let filteredListData: [CityData] = data.filter({ (object) -> Bool in
+            searchedText.isEmpty ? true : object.name.contains(searchedText)
+        })
+        // можно ли это как-то вынести?
+        filteredData = filteredListData
+        tableView.reloadData()
+    }
+    // MARK: - Search Logic: Binary
+    func getFilteredDataBinary(searchedText: String) {
+        
+        guard !searchedText.isEmpty else {
+            filteredData = data
+            tableView.reloadData()
+            return
+        }
+        
+        var lowerBound = 0
+        var upperBound = data.count
+        var randomValidMidIndex: Int?
+        
+        while lowerBound < upperBound {
+            let midIndex = lowerBound + (upperBound - lowerBound) / 2
+            if data[midIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
+                randomValidMidIndex = midIndex
+                break
+            } else if data[midIndex].name.lowercased() < searchedText.lowercased()  {
+                lowerBound = midIndex + 1
+            } else {
+                upperBound = midIndex
+            }
+        }
+        
+        guard let midIndex = randomValidMidIndex else { return }
+        
+        var startIndex = midIndex
+        var endIndex = midIndex
+        
+        while startIndex > -1, data[startIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
+            startIndex -= 1
+        }
+        
+        while endIndex < data.count, data[endIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
+            endIndex += 1
+        }
+        
+        filteredData = Array(data[(startIndex + 1)..<endIndex])
+        tableView.reloadData()
+    }
 }
 
--- DataFromJSONfile/View/CityTable.swift
+++ DataFromJSONfile/View/CityTable.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 import PureLayout
 
 class CityTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -20,48 +21,28 @@ class CityTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let searchBarController = UISearchController(searchResultsController: nil)
     var segmentControl: UISegmentedControl!
     
-    @IBOutlet weak var segmentControlOutlet: UISegmentedControl!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        addControl()
+        addSegmentControl()
         setSearchBarUI()
         getFilteredDataBinary(searchedText: "")
+        tableViewSetup()
+    }
+    
+    // MARK: - Table: view data source
+    func tableViewSetup() {
         tableView = UITableView(frame: self.view.bounds, style: UITableView.Style.plain)
         self.tableView.register(myCell.self, forCellReuseIdentifier: cellID)
         self.tableView.dataSource = self
         self.tableView.delegate = self
         self.view.addSubview(tableView)
-        
     }
-    // MARK: - Selector
-    func addControl() {
-        let segmentItems = ["Binary", "Filter"]
-        segmentControl = UISegmentedControl(items: segmentItems)
-        segmentControl.addTarget(self, action: #selector(selector(_:)), for: .valueChanged)
-        segmentControl.selectedSegmentIndex = 0
-        view.addSubview(segmentControl)
-        navigationItem.titleView = segmentControl
-    }
-    
-    @objc func selector(_ sender: UISegmentedControl) {
-        switch segmentControl.selectedSegmentIndex {
-        case 0:
-            getFilteredDataBinary(searchedText: "")
-        case 1:
-            getFilteredData()
-        default:
-            break
-        }
-    }
-    // MARK: - Table: view data source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredData.count
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
     }
-    
     // MARK: - Table: placing data in cells
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: myCell? = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? myCell
@@ -80,9 +61,7 @@ class CityTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
         mapVC.city = filteredData[indexPath.row]
         navigationController?.pushViewController(mapVC, animated: true)
     }
-    
-    // MARK: - searchbar configuration
-    
+    // MARK: - Searchbar configuration
     func setSearchBarUI() {
         searchBarController.searchBar.delegate = self
         searchBarController.obscuresBackgroundDuringPresentation = false
@@ -90,58 +69,26 @@ class CityTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
         navigationItem.searchController = searchBarController
         navigationItem.hidesSearchBarWhenScrolling = false
     }
-    
-    // MARK: - search logic: filter
-    
-    func getFilteredData(searchedText: String = "") {
-        let filteredListData: [CityData] = data.filter({ (object) -> Bool in
-            searchedText.isEmpty ? true : object.name.contains(searchedText)
-        })
-        // можно ли это как-то вынести?
-        filteredData = filteredListData
-        tableView.reloadData()
+    // MARK: - Selector
+    func addSegmentControl() {
+        let segmentItems = ["Binary", "Filter"]
+        segmentControl = UISegmentedControl(items: segmentItems)
+        segmentControl.addTarget(self, action: #selector(selector(_:)), for: .valueChanged)
+        segmentControl.selectedSegmentIndex = 0
+        view.addSubview(segmentControl)
+        navigationItem.titleView = segmentControl
     }
-    
-    // MARK: - search logic: binary
-    
-    func getFilteredDataBinary(searchedText: String) {
-        
-        guard !searchedText.isEmpty else {
-            filteredData = data
-            tableView.reloadData()
-            return
+    @objc func selector(_ sender: UISegmentedControl) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            getFilteredDataBinary(searchedText: "")
+        case 1:
+            getFilteredData()
+        default:
+            break
         }
-        
-        var lowerBound = 0
-        var upperBound = data.count
-        var randomValidMidIndex: Int?
-        
-        while lowerBound < upperBound {
-            let midIndex = lowerBound + (upperBound - lowerBound) / 2
-            if data[midIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
-                randomValidMidIndex = midIndex
-                break
-            } else if data[midIndex].name.lowercased() < searchedText.lowercased()  {
-                lowerBound = midIndex + 1
-            } else {
-                upperBound = midIndex
-            }
-        }
-        
-        guard let midIndex = randomValidMidIndex else { return }
-        
-        var startIndex = midIndex
-        var endIndex = midIndex
-        
-        while startIndex > -1, data[startIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
-            startIndex -= 1
-        }
-        
-        while endIndex < data.count, data[endIndex].name.lowercased().hasPrefix(searchedText.lowercased()) {
-            endIndex += 1
-        }
-        
-        filteredData = Array(data[(startIndex + 1)..<endIndex])
-        tableView.reloadData()
     }
 }
+
+
+
